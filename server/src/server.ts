6965f6a5270dd4cbf7b42f0d3140dbce2164bc36
Anna Nayara Lira <anna_nayara@hotmail.com>
2023-05-19 14:18:52 -0300

    config iniciais do fastify

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,13 @@
-console.log("oi");
+import fastify from "fastify";
+const app = fastify()
+
+app.get("/hello", () => {
+    return "Hello World"
+})
+
+app.listen({
+    port: 3333,
+}).then( () => {
+    console.log(`HTTP server running on http://localhost:3333`);
+    
+})
