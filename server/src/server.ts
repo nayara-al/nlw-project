5d191ca29c37b7eb3dd0fe06ac790e0fc9eaf587
Anna Nayara Lira <anna_nayara@hotmail.com>
2023-05-22 10:46:19 -0300

    cors config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,15 @@
 import fastify from "fastify";
-const app = fastify()
+import { PrismaClient } from "@prisma/client"
+import cors from "@fastify/cors"
 
-app.get("/hello", () => {
-    return "Hello NLW"
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get("/hello", async () => {
+    const habits = prisma.habit.findMany()
+    return habits
 })
 
 app.listen({
